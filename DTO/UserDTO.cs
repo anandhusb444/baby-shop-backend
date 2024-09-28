4c42a681609d188e7235c32b1dc58f844d07bb3f
anandhusb444 <anandhusb444@gmail.com>
2024-09-28 17:46:21 +0530

    Add userDTO

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -1,24 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace baby_shop_backend.Models.UserModel
+namespace baby_shop_backend.DTO
 {
-    public class User
+    public class UserDTO
     {
-        [Key]
-        public int id { get; set; }
         [Required]
         public string userName { get; set; }
         [Required]
-        [EmailAddress]
         public string userEmail { get; set; }
-
         [Required]
         [StringLength(100,MinimumLength = 8)]
         public string password { get; set; }
-
-        public bool isStatus { get; set; } = true;
-        public string Role { get; set; } = "User";
-
-
     }
 }
