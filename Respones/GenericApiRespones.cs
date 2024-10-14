06176a56a1e3577d49f4367528a07b1b5d78bca3
anandhusb444 <anandhusb444@gmail.com>
2024-10-14 12:21:12 +0530

    create a Generic class for api respones

---
+++ Respones/GenericApiRespones.cs
@@ -0,0 +1,21 @@
+﻿namespace baby_shop_backend.Respones
+{
+    public class GenericApiRespones<T>
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public string Error { get; set; }
+
+        public GenericApiRespones(int statuscode,string msg,T data,string error)
+        {
+            StatusCode = statuscode;
+            Message = msg;
+            Data = data;
+            Error = error;
+        }
+
+
+
+    }
+}
